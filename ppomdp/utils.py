487bb6578c3c0f7c25ffda44634fcb0be3fba253
hanyas <hanyas@users.noreply.github.com>
2024-10-25 11:36:43 +0300

    add generic recurrent policy

--- ppomdp/utils.py
+++ ppomdp/utils.py
@@ -8,10 +8,13 @@ from distrax import MultivariateNormalDiag
 from distrax import Transformed, Block, Chain
 
 
-class GaussianPolicy(nn.Module):
+LSTMCarry = tuple[Array, Array]
+
+
+class GaussianMLP(nn.Module):
     dim: int
     layer_size: Sequence[int]
-    transform: Callable
+    feature_fn: Callable
     activation: Callable
     init_log_std: Callable = nn.initializers.ones
     init_kernel: Callable = nn.initializers.he_uniform()
@@ -20,11 +23,50 @@ class GaussianPolicy(nn.Module):
     def __call__(self, s):
         log_std = self.param('log_std', self.init_log_std, self.dim)
 
-        y = self.transform(s)
+        y = self.feature_fn(s)
         for s in self.layer_size:
             y = self.activation(nn.Dense(s, self.init_kernel)(y))
         a = nn.Dense(self.dim)(y)
         return a
+
+
+class GaussianLSTM(nn.Module):
+    """An LSTM policy with a dense input and output layers."""
+    dim: int
+    feature_fn: Callable
+    encoder_size: Sequence[int]
+    recurr_size: Sequence[int]
+    output_size: Sequence[int]
+    init_log_std: Callable = nn.initializers.ones
+    init_kernel: Callable = nn.initializers.he_uniform()
+
+    @nn.compact
+    def __call__(self, carry: LSTMCarry, s: Array) -> tuple[LSTMCarry, Array]:
+        log_std = self.param('log_std', self.init_log_std, self.dim)
+
+        # pass inputs through features layer
+        y = self.feature_fn(s)
+
+        # pass features through encoding layers
+        for _size in self.encoder_size:
+            y = nn.relu(nn.Dense(_size, self.init_kernel)(y))
+        y = nn.Dense(self.recurr_size[0])(y)
+
+        # pass encodings through recurrent layers
+        for _size in self.recurr_size:
+            carry, y = nn.LSTMCell(_size)(carry, y)
+
+        # pass result through output layers
+        for _size in self.output_size:
+            y = nn.relu(nn.Dense(_size, self.init_kernel)(y))
+        a = nn.Dense(self.dim)(y)
+        return carry, a
+
+
+def initialize_carry(model: GaussianLSTM, input_shape: tuple[int, ...]) -> LSTMCarry:
+    batch_dims = input_shape[:-1]
+    mem_shape = batch_dims + (model.lstm_size,) + (model.lstm_size,)  # accounts for 2 cells
+    return jnp.zeros(mem_shape), jnp.zeros(mem_shape)
 
 
 def policy_distribution(
