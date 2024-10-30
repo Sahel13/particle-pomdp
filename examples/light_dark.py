d4dfa763b9a723446daef6266d0f72e03a18b2f6
hanyas <hanyas@users.noreply.github.com>
2024-10-30 14:57:54 +0200

    refactor light dark example

--- examples/light_dark.py
+++ examples/light_dark.py
@@ -5,10 +5,8 @@ import jax
 from jax import random
 from jax import Array
 import jax.numpy as jnp
-
 from chex import PRNGKey
 
-from flax import linen as nn
 from flax.training.train_state import TrainState
 
 from distrax import MultivariateNormalDiag
@@ -16,13 +14,16 @@ from distrax import Chain, ScalarAffine
 
 import optax
 
-from ppomdp.utils import StochasticLSTM, LSTMCarry
-from ppomdp.utils import initialize_carry, lstm_distribution
+from ppomdp.core import LSTMCarry
+from ppomdp.utils import (
+    LSTM,
+    initialize_carry,
+    lstm_distribution
+)
 
 
 def transition_mean(s: Array, a: Array) -> Array:
-    dt = 0.1
-    return s + dt * a
+    return s + 0.1 * a
 
 
 def transition_stddev(s: Array, a: Array) -> Array:
@@ -82,7 +83,7 @@ def unit_features(s: Array) -> Array:
     return s
 
 
-policy = StochasticLSTM(
+lstm = LSTM(
     dim=1,
     feature_fn=unit_features,
     encoder_size=[256, 256],
@@ -95,32 +96,36 @@ bijector = Chain([
 ])
 
 
-def policy_sample(rng_key: PRNGKey, s: Array, params: Dict, carry: LSTMCarry) -> Array:
-    dist = lstm_distribution(s, policy, params, carry, bijector)
-    return dist.sample(seed=rng_key)
+def sample_policy(
+    rng_key: PRNGKey, s: Array, carry: list[LSTMCarry], params: Dict
+) -> tuple[list[LSTMCarry], Array]:
+    carry, dist = lstm_distribution(s, carry, lstm, params, bijector)
+    return carry, dist.sample(seed=rng_key)
 
 
-def policy_logpdf(a: Array, s: Array, params: Dict, carry: LSTMCarry) -> float:
-    dist = lstm_distribution(s, policy, params, carry, bijector)
+def log_prob_policy(
+    a: Array, s: Array, carry: list[LSTMCarry], params: Dict
+) -> float:
+    _, dist = lstm_distribution(s, carry, lstm, params, bijector)
     return dist.log_prob(a)
 
 
 def create_train_state(
-    rng_key: PRNGKey, module: StochasticLSTM, optimizer: Callable, learning_rate: float
+    rng_key: PRNGKey, module: LSTM, optimizer: Callable, learning_rate: float
 ) -> TrainState:
     init_key, param_key = random.split(rng_key)
     init_data = random.normal(init_key, (1,))
-    init_carry = initialize_carry(module, (1,))
+    init_carry = initialize_carry(module, 1)
     params = module.init(param_key, init_carry, init_data)["params"]
     tx = optimizer(learning_rate)
     return TrainState.create(apply_fn=module.apply, params=params, tx=tx)
 
 
 @partial(jax.jit, static_argnums=1)
-def train_step(state: TrainState, module: StochasticLSTM, data: tuple[Array, Array]):
+def train_step(state: TrainState, module: LSTM, data: tuple[Array, Array]):
     def loss_fn(params):
         apply_fn = partial(state.apply_fn, {"params": params})
-        init_carry = initialize_carry(module, (1,))
+        init_carry = initialize_carry(module, 1)
         _, predictions = jax.lax.scan(apply_fn, init_carry, data[0])
         return optax.squared_error(predictions, data[1]).mean()
 
