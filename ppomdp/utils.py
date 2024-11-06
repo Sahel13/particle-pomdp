b9223aa04e432727d33581e6f68fb69f453056dd
Sahel Iqbal <sahel13miqbal@proton.me>
2024-11-06 15:26:24 +0200

    Clean up batch_data

--- ppomdp/utils.py
+++ ppomdp/utils.py
@@ -1,8 +1,7 @@
-from jax import Array
-from jax import numpy as jnp
-from jax import random
-
 import math
+
+from jax import Array, random
+from jax import numpy as jnp
 
 
 def batch_data(
@@ -10,30 +9,27 @@ def batch_data(
     data_size: int,
     batch_size: int,
     skip_last: bool = False,
-) -> Array:
-    """
-    Generates batches of data from the given dataset.
+) -> list[Array]:
+    """Generates batched indices.
 
     Args:
         rng_key (Array): Random key for shuffling the data.
-        data_size (int): The size dataset to be batched.
+        data_size (int): The size of the dataset to be batched.
         batch_size (int): The size of each batch.
         skip_last (bool, optional): If True, skips the last incomplete batch. Defaults to False.
 
     Yields:
-        Array: A batch of data.
+        list[Array]: A list of batched indices.
     """
     batch_idx = random.permutation(rng_key, data_size)
 
     if skip_last:
         # Skip incomplete batch
-        samples_per_epoch = data_size // batch_size
-        batch_idx = batch_idx[: samples_per_epoch * batch_size]
-        batch_idx = batch_idx.reshape((samples_per_epoch, batch_size))
+        num_batches = data_size // batch_size
+        batch_idx = batch_idx[: num_batches * batch_size]
     else:
-        # include incomplete batch
-        samples_per_epoch = math.ceil(data_size / batch_size)
-        batch_idx = jnp.array_split(batch_idx, samples_per_epoch)
+        # Include incomplete batch
+        num_batches = math.ceil(data_size / batch_size)
 
-    for idx in batch_idx:
-        yield idx
+    batch_idx = jnp.array_split(batch_idx, num_batches)
+    return batch_idx
