d9202055cc740cf4bacc94fab844fdd499151379
Sahel Iqbal <sahel13miqbal@proton.me>
2025-01-20 15:26:48 +0200

    Add environments

--- ppomdp/envs/__init__.py
+++ ppomdp/envs/__init__.py
@@ -1,2 +1,4 @@
 from ppomdp.envs.base import Environment
 from ppomdp.envs.pendulum import PendulumEnv
+from ppomdp.envs.cartpole import CartPoleEnv
+from ppomdp.envs.light_dark_two_dim import LightDarkTwoEnv
