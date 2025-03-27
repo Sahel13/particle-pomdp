bb9171286e1e0786cd4b3b80ab83e1fb7e0898c3
hanyas <hanyas@users.noreply.github.com>
2025-03-27 21:18:36 +0100

    update dsmc example

--- baselines/examples/dsmc/pendulum.py
+++ baselines/examples/dsmc/pendulum.py
@@ -12,6 +12,7 @@ from baselines.dsmc.dsmc import (
     create_train_state,
     step_and_train,
 )
+from baselines.dvrl.utils import belief_init, belief_update
 from ppomdp.envs.pomdps import PendulumEnv as env_obj
 
 import matplotlib.pyplot as plt
@@ -95,46 +96,48 @@ if __name__ == "__main__":
             + f"Policy log std: {train_state.policy_state.params['log_std'][0]:6.2f}"
         )
 
-    # # Evaluate the learned policy.
-    # key, state_key, obs_key = random.split(key, 3)
-    # state = env_obj.prior_dist.sample(seed=state_key)
-    # observation = env_obj.obs_model.sample(obs_key, state)
-    # lstm_carry = policy_network.reset(1)
-    #
-    # def body(carry, rng_key):
-    #     _state, _lstm_carry, _observation = carry
-    #     _action_key, _state_key, _obs_key = random.split(rng_key, 3)
-    #     _lstm_carry, _, _, _action = train_state.policy_state.apply_fn(
-    #         rng_key=_action_key,
-    #         params=train_state.policy_state.params,
-    #         carry=_lstm_carry,
-    #         observation=_observation
-    #     )
-    #     _state = env_obj.trans_model.sample(_state_key, _state, _action[0])
-    #     _observation = env_obj.obs_model.sample(_obs_key, _state)
-    #     return (_state, _lstm_carry, _observation), (_state, _observation, _action[0])
+    # Evaluate the learned policy.
+    key, obs_key, belief_key = random.split(key, 3)
+    state = env_obj.prior_dist.mean()
+    observation = env_obj.obs_model.sample(obs_key, state)
+    belief_state = belief_init(belief_key, env_obj, observation, config.num_belief_particles)
 
-    # keys = random.split(key, env_obj.num_time_steps)
-    # _, (states, _, actions) = jax.lax.scan(
-    #     body, (state, lstm_carry, observation), keys
-    # )
-    # states = jnp.concatenate([state[None, ...], states], axis=0)
-    #
-    # fig, axs = plt.subplots(3, 1, figsize=(10, 10))
-    # fig.suptitle("Simulated trajectory")
-    #
-    # axs[0].plot(states[:, 0])
-    # axs[0].set_ylabel("Angle")
-    # axs[0].grid(True)
-    #
-    # axs[1].plot(states[:, 1])
-    # axs[1].set_ylabel("Angular velocity")
-    # axs[1].grid(True)
-    #
-    # axs[2].plot(actions[:, 0])
-    # axs[2].set_ylabel("Action")
-    # axs[2].set_xlabel("Time")
-    # axs[2].grid(True)
-    #
-    # plt.tight_layout()
-    # plt.show()
+    def body(carry, rng_key):
+        _state, _belief_state = carry
+        _action_key, _state_key, _obs_key, _pf_key = random.split(rng_key, 4)
+        _, _, _action = \
+            train_state.policy_state.apply_fn(
+                rng_key=_action_key,
+                params=train_state.policy_state.params,
+                particles=_belief_state.particles,
+                weights=_belief_state.weights,
+        )
+        _state = env_obj.trans_model.sample(_state_key, _state, _action)
+        _observation = env_obj.obs_model.sample(_obs_key, _state)
+        _belief_state = belief_update(_pf_key, env_obj, _belief_state, _observation, _action)
+        return (_state, _belief_state), (_state, _action)
+
+    keys = random.split(key, env_obj.num_time_steps)
+    _, (states, actions) = jax.lax.scan(
+        body, (state, belief_state), keys
+    )
+    states = jnp.concatenate([state[None, ...], states], axis=0)
+
+    fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+    fig.suptitle("Simulated trajectory")
+
+    axs[0].plot(states[:, 0])
+    axs[0].set_ylabel("Angle")
+    axs[0].grid(True)
+
+    axs[1].plot(states[:, 1])
+    axs[1].set_ylabel("Angular velocity")
+    axs[1].grid(True)
+
+    axs[2].plot(actions[:, 0])
+    axs[2].set_ylabel("Action")
+    axs[2].set_xlabel("Time")
+    axs[2].grid(True)
+
+    plt.tight_layout()
+    plt.show()
