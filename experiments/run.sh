4ceff2091967ce572cbb962cad4fa678c550a6e4
Sahel Iqbal <sahel13miqbal@proton.me>
2025-03-28 15:11:47 +0200

    Post process data for plotting

--- experiments/run.sh
+++ experiments/run.sh
@@ -3,7 +3,7 @@
 # Specify which GPU to use.
 export CUDA_VISIBLE_DEVICES=0
 
-# Arguments for the script.
+# Command line arguments for the script.
 algorithm=${1:?No algorithm specified. Usage: ./run.sh <algorithm> <environment>}
 environment=${2:?No environment specified. Usage: ./run.sh <algorithm> <environment>}
 
@@ -12,7 +12,10 @@ log_dir="logs/${algorithm}_${environment}_$(date +%Y%m%d_%H%M%S)"
 mkdir -p "$log_dir"
 
 # Loop over seeds.
-for seed in {0..1}
+for seed in {0..9}
 do
     python "$algorithm.py" --seed "$seed" --env "$environment" --log_dir "$log_dir"
 done
+
+# Process the data for plotting.
+python post_process.py --folder "./$log_dir"
