dc8ce4adb74fb78290d3f4ac3c25bd54afbd3c6e
Sahel Mohammad Iqbal <sahel13miqbal@proton.me>
2025-05-02 19:07:12 +0300

    Update slurm script

--- experiments/slurm.sh
+++ experiments/slurm.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-
-algo="$1"
-
-case "$algo" in
-  slac|dsmc|dvrl)
-    # allowed, continue
-    ;;
-  *)
-    echo "Error: Invalid input '$algo'. Allowed values are: slac, dsmc, dvrl." >&2
-    exit 1
-    ;;
-esac
-
-#SBATCH --job-name=${algo}_%A # Job name includes algo and array ID
-#SBATCH --output=${algo}_%A_%a.out # Output file includes algo name
-#SBATCH --error=${algo}_%A_%a.err  # Error file includes algo name
+#SBATCH --job-name=slac_%A # Job name includes algo and array ID
+#SBATCH --output=slac_%A_%a.out # Output file includes algo name
+#SBATCH --error=slac_%A_%a.err  # Error file includes algo name
 #SBATCH --mail-type=end
 #SBATCH --mail-user=sahel.iqbal@aalto.fi
 
@@ -24,21 +11,21 @@ esac
 # Resources PER ARRAY TASK:
 #SBATCH --ntasks=1           # Each array task runs 1 process
 #SBATCH --cpus-per-task=8    # CPUs for that process
-#SBATCH --gres=gpu:v100:1    # 1 V100 GPU for that process
-#SBATCH --mem=8000M             # Memory for that process (8GB)
-#SBATCH --time=03:59:59      # Time limit for EACH array task
+#SBATCH --gres=gpu:a100:1    # 1 V100 GPU for that process
+#SBATCH --mem=6000M             # Memory for that process (8GB)
+#SBATCH --time=02:00:00      # Time limit for EACH array task
 
 # Generate a unique run ID (same for all tasks in the array)
-EXPERIMENT_ID=$(uuidgen)
+EXPERIMENT_ID="202505021602"
 
 module purge
 module load mamba
-mamba activate particle-pomdp
+source activate particle-pomdp
 
-python ${algo}_experiment.py \
+python slac_experiment.py \
   --env_id cartpole \
+  --starting-seed ${SLURM_ARRAY_TASK_ID} \
   --num_seeds 1 \
   --cuda_device 0 \
   --experiment_group slac-cartpole \
-  --experiment_id ${EXPERIMENT_ID} \
-  --starting_seed ${SLURM_ARRAY_TASK_ID}
+  --experiment_id ${EXPERIMENT_ID}
